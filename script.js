8bf2913d3e1a3a1cb84aaab62f667ad49815b3c4
nils_hackstein <nils.hackstein2@gmail.com>
2020-02-24 11:12:20 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,24 @@
+const todoItems = document.getElementsByClassName("todoItem");
+var ul = document.getElementById("todoItems");
+const ueberschrift = document.getElementById("ueberschrift");
+const todoHinzufuegen = document.getElementById("todoHinzufuegen");
+let newItemCounter = 1;
+
+todoHinzufuegen.addEventListener('click', hinzufuegen)
+
+ul.addEventListener('mouseover', highlightItem);
+
+function highlightItem(e){
+    if(e.target.nodeName == "LI"){
+        for(let j = 0; j < e.target.parentNode.children.length; j++){
+            e.target.parentNode.children[j].classList.remove('active');
+        }
+      e.target.classList.add('active');
+    }
+   
+}
+
+function hinzufuegen(){
+  ul.innerHTML += "<li>Item" + newItemCounter + "</li>";
+  newItemCounter++;
+}
