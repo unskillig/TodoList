a3fb26a80723ffc2230dd42b7f00740aa8c34fac
nils_hackstein <nils.hackstein2@gmail.com>
2020-02-24 13:33:47 +0100

    Hover over Elements implemented

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ let newItemCounter = 1;
 
 todoHinzufuegen.addEventListener('click', hinzufuegen)
 
-ul.addEventListener('click', highlightItem);
+ul.addEventListener('mouseover', highlightItem);
+
+document.body.addEventListener('mouseover', unhighlightItem);
 
 function highlightItem(e){
+  // falls geklicktes Element Listenelement
     if(e.target.nodeName == "LI"){
-     
-    
         for(let j = 0; j < e.target.parentNode.children.length; j++){
           e.target.parentNode.children[j].classList.remove('active');
       }  
       e.target.classList.add('active');
     }
-   
 }
 
+
+function unhighlightItem(e){
+  if(e.target.nodeName != "LI"){
+    var listenelemente = document.getElementById("todoItems").children;
+    for(let k = 0; k < listenelemente.length; k++){
+      listenelemente[k].classList.remove('active');
+    }
+  }
+}
+
+
 function hinzufuegen(){
-  ul.innerHTML += "<li>Item" + newItemCounter + "</li>";
+  const newItem = document.createElement("li");
+  newItem.setAttribute('class', "todoItem");
+  newItem.innerHTML = "New Item " + newItemCounter.toString();
   newItemCounter++;
+  ul.appendChild(newItem);
 }
